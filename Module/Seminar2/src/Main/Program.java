2d7fe7732d638a29fbfa467f543d593ccba1a157
morarup <morarupaul20@stud.ase.ro>
2024-02-28 22:15:19 +0200

    Seminar2

---
+++ Module/Seminar2/src/Main/Program.java
@@ -0,0 +1,27 @@
+package Main;
+
+import clase.Aplicant;
+import clase.readers.classes.AngajatReader;
+import clase.readers.interfaces.ApplicantReader;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+
+public class Program {
+
+    public static void main(String[] args) {
+        List<Aplicant> listaAngajati;
+        ApplicantReader angajatReader = new AngajatReader("angajati.txt");
+
+        try {
+            listaAngajati = angajatReader.read();
+            for(Aplicant angajat : listaAngajati)
+                System.out.println(angajat.toString());
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
